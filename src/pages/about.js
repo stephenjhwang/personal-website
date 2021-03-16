a83e6d38bf633c4a5bafc07886dcdc6050d26f9a
Stephen <stephenhwang156@outlook.com>
2021-03-15 23:11:09 -0400

    Added gatsby project

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very React.</p>
+      <Link to="/">naaadex</Link>
+    </div>
+  )
+}
