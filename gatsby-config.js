328b8d344e28039e8122ee29e1b02cdb27670ce2
Stephen <stephenhwang156@outlook.com>
2021-03-19 01:50:46 -0400

    Added styled components plugin, updated index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,10 @@
 module.exports = {
     plugins: [
-
+        {
+            resolve: `gatsby-plugin-styled-components`,
+            options: {
+              // Add any options here
+            },
+        },
     ]
 }
