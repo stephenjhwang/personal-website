6e972ec32ba6bb3f6b596805ba03d0a45b17cdec
Stephen Hwang <stephenhwang156@outlook.com>
2021-05-03 00:48:23 -0400

    about page done

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,17 +7,19 @@ import '../css/index.scss'
 
 const Layout = styled.main`
   display: grid;
-  height: calc(100vh - 64px - 10%);
+  height: calc(100vh - 100px);
+  max-height: 700px;
   grid-template-columns: repeat(3, 1fr);
-  grid-auto-rows: 200px;
+  grid-auto-rows: 200px 1fr;
   max-width: 1100px;
   margin: auto;
   align-items: center;
-  padding: 5% 20px;
+  padding: 50px 20px;
   gap: 20px;
 
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: 150px 1fr 1fr;
   }
 `
 
@@ -41,6 +43,8 @@ const TextContainer = styled.div`
 
 const Text = styled.p`
   font-size: 2rem;
+  line-height: 3rem;
+  overflow: auto;
 `
 
 export default function About() {
@@ -58,7 +62,7 @@ export default function About() {
         </ImageWrapper>
         <TextContainer>
           <Text>
-            Hello, I am a 4th year Computer Science student at the Univeristy of Waterloo.
+            Hello, I am a 4th year Computer Science student at the University of Waterloo. Besides the yada yada of life, I enjoy yeeting around and getting lit shiettt.
           </Text>
         </TextContainer>
       </Layout>
